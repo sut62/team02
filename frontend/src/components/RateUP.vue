5bf9d3409a9d24eb3da86309a2ae4ccf1dcf1385
Kritzer <kissyoukissme93@gmail.com>
2019-12-10 16:16:09 +0700

    ไฟล์โปรเจ็คส่วน frontend

---
+++ frontend/src/components/RateUP.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app>
+        <v-card>
+            <v-container>
+                
+            </v-container>
+        </v-card>
+    </v-app>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
