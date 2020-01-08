1b1e40ec5d2812a69d3ac6bdef38ca964cca337f
Payak Promnok <b5923403@office365.sut.ac.th>
2020-01-09 01:14:20 +0700

    close #48 - เพิ่ม repository ของระบบยืมหนังสือ

--- backend/src/main/java/com/example/demo/repository/DocumentRepository.java
+++ backend/src/main/java/com/example/demo/repository/DocumentRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface DocumentRepository extends JpaRepository<Document, Long> {
-    
+    public Document findById(long documentid);
 }    //JPA เป็นตัว map class ให้เป็น Database
