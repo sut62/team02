866a92170f3df1fa730c2b7b20e658b1d5415560
Payak Promnok <b5923403@office365.sut.ac.th>
2020-01-10 18:00:05 +0700

    close #52 - ทำpop up แสดงข้อความ

--- frontend/src/components/borrow.vue
+++ frontend/src/components/borrow.vue
@@ -3,7 +3,7 @@
     <!-- <v-app-bar app color="blue darken-2" dark>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>ระบบห้องสมุด</v-toolbar-title>
-    </v-app-bar> -->
+    </v-app-bar>-->
     <v-container>
       <div>
         <v-responsive>
@@ -11,21 +11,20 @@
             <v-card class="mx-auto" max-width="80%">
               <v-toolbar color="blue darken-2" dark flat>
                 <v-toolbar-title>ระบบยืมหนังสือ</v-toolbar-title>
-
               </v-toolbar>
               <v-card-text>
                 <v-form>
                   <v-row>
                     <v-col cols="12">
                       <v-select
-                              label="เลือกชื่อสมาชิก"
-                              outlined
-                              v-model="borrow.memberId"
-                              :items="Member"
-                              item-text="name"
-                              item-value="memberid"
-                              :rules="[(v) => !!v || 'Item is required']"
-                              required
+                        label="เลือกชื่อสมาชิก"
+                        outlined
+                        v-model="borrow.memberId"
+                        :items="Member"
+                        item-text="name"
+                        item-value="memberid"
+                        :rules="[(v) => !!v || 'Item is required']"
+                        required
                       ></v-select>
                     </v-col>
                   </v-row>
@@ -33,14 +32,14 @@
                   <v-row>
                     <v-col cols="12">
                       <v-select
-                              label="เลือกประเภทหนังสือ"
-                              outlined
-                              v-model="borrow.booktypeId"
-                              :items="BookTypes"
-                              item-text="bookTypeName"
-                              item-value="id"
-                              :rules="[(v) => !!v || 'Item is required']"
-                              required
+                        label="เลือกประเภทหนังสือ"
+                        outlined
+                        v-model="borrow.booktypeId"
+                        :items="BookTypes"
+                        item-text="bookTypeName"
+                        item-value="id"
+                        :rules="[(v) => !!v || 'Item is required']"
+                        required
                       ></v-select>
                     </v-col>
                   </v-row>
@@ -48,45 +47,49 @@
                   <v-row>
                     <v-col cols="12">
                       <v-select
-                              label="เลือกชื่อหนังสือ"
-                              outlined
-                              v-model="borrow.booknameId"
-                              :items="Documents"
-                              item-text="bookName"
-                              item-value="id"
-                              :rules="[(v) => !!v || 'Item is required']"
-                              required
+                        label="เลือกชื่อหนังสือ"
+                        outlined
+                        v-model="borrow.booknameId"
+                        :items="Documents"
+                        item-text="bookName"
+                        item-value="id"
+                        :rules="[(v) => !!v || 'Item is required']"
+                        required
                       ></v-select>
                     </v-col>
                   </v-row>
 
                   <v-col cols="12">
                     <v-text-field
-                            label="จำนวนเล่ม"
-                            name="name"
-                            type="text"
-                            v-model="borrow.numbers"
-                            :rules="[(v) => !!v || 'Please fill in the information']"
-                            required
+                      label="จำนวนเล่ม"
+                      name="name"
+                      type="text"
+                      v-model="borrow.numbers"
+                      :rules="[(v) => !!v || 'Please fill in the information']"
+                      required
                     ></v-text-field>
                   </v-col>
 
                   <v-row>
                     <v-col cols="12">
                       <v-select
-                              label="เลือกประเภทสมาชิก"
-                              outlined
-                              v-model="borrow.memtpeId"
-                              :items="Memtype"
-                              item-text="memtype"
-                              item-value="memtypeid"
-                              :rules="[(v) => !!v || 'Item is required']"
-                              required
+                        label="เลือกประเภทสมาชิก"
+                        outlined
+                        v-model="borrow.memtpeId"
+                        :items="Memtype"
+                        item-text="memtype"
+                        item-value="memtypeid"
+                        :rules="[(v) => !!v || 'Item is required']"
+                        required
                       ></v-select>
                     </v-col>
                   </v-row>
-
                 </v-form>
+
+                <v-snackbar v-model="snackbar">
+                  {{ text }}
+                  <v-btn color="pink" text @click="snackbar = false">Close</v-btn>
+                </v-snackbar>
               </v-card-text>
               <v-card-actions>
                 <v-btn color="error" @click="clear">Clear</v-btn>
@@ -114,15 +117,15 @@ export default {
         booknameId: "",
         numbers: "",
         memtpeId: ""
-      }
-      
-      
+      },
+      snackbar: false,
+      text: ""
     };
   },
   methods: {
     /* eslint-disable no-console */
     // ดึงข้อมูล Employee ใส่ combobox
-     getCreatMember() {
+    getCreatMember() {
       http
         .get("/member")
         .then(response => {
@@ -172,41 +175,41 @@ export default {
           console.log(e);
         });
     },
-    
+
     saveBorrow() {
-      console.log(this.borrow)
+      console.log(this.borrow);
       http
-        .post(//borrow/{numbers}/{member_id}/{bookType_id}/{document_id}/{memType_id}"
+        .post(
+          //borrow/{numbers}/{member_id}/{bookType_id}/{document_id}/{memType_id}"
           "/borrow/" +
             parseInt(this.borrow.numbers, 10) +
-            "/" +     
+            "/" +
             this.borrow.memberId +
             "/" +
             this.borrow.booktypeId +
             "/" +
             this.borrow.booknameId +
-            "/" + 
+            "/" +
             this.borrow.memtpeId
-            
         )
         .then(response => {
           console.log(response);
-          alert('เพิ่มข้อมูลสำเร็จ');
+          this.text = "เพิ่มข้อมูลสำเร็จ"
+          this.snackbar = !this.snackbar
         })
         .catch(e => {
           console.log(e);
-          alert('ไม่สามารถเพิ่มข้อมูลได้');
-        });
+          this.text = "ไม่สามารถเพิ่มข้อมูลได้"
+          this.snackbar = !this.snackbar
+        })
       this.submitted = true;
     },
     clear() {
-      this.borrow.memberId = ''
-      this.borrow.booktypeId = ''
-      this.borrow.booknameId = ''
-      this.borrow.numbers = ''
-      this.borrow.memtpeId = ''
-     
-      
+      this.borrow.memberId = "";
+      this.borrow.booktypeId = "";
+      this.borrow.booknameId = "";
+      this.borrow.numbers = "";
+      this.borrow.memtpeId = "";
     },
     refreshList() {
       this.getCreatMember();
