ff693a23ece275737ecf4246933c0e761d95d5a6
unknown <kamonpan.b5923144@gmail.com>
2020-01-25 18:19:15 +0700

    แก้หน้าUIแสดงรายชื่อ - close #88

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,33 @@
 <template>
+
   <v-app>
-    <v-app-bar app color="blue darken-2" dark>
+    <v-app-bar app color="pink" dark  src="">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      
       <v-toolbar-title>ระบบห้องสมุด</v-toolbar-title>
+        <v-toolbar-items>
+         <v-divider inset vertical></v-divider>
+         
+      
+
+
+      </v-toolbar-items>
+
         <div class="flex-grow-1"></div>
 
+
       <v-toolbar-items>
-        
-        <v-btn text to="/rates">ให้คะแนน</v-btn>
-        <v-btn text to="/Member">สมาชิก</v-btn>
-        <v-btn text to="/addVideo">เพิ่มวิดีโอ</v-btn>
-        <v-btn text to="/AddBook">เพิ่มหนังสือ</v-btn>
-        <v-btn text to="/borrow">ยืมหนังสือ</v-btn>
-        <v-btn text to="/remand">คืนหนังสือ</v-btn>
-        
+        <v-btn text to="/home">Home</v-btn> <v-divider inset vertical></v-divider>
+         <v-divider inset vertical></v-divider>
+         <v-btn text to="/Member">ลงทะเบียนสมาชิก</v-btn> <v-divider inset vertical></v-divider>
+        <v-btn text to="/AddBook">เพิ่มหนังสือ</v-btn> <v-divider inset vertical></v-divider>
+        <v-btn text to="/addVideo">เพิ่มวิดีโอ</v-btn> <v-divider inset vertical></v-divider>
+        <v-btn text to="/borrow">ยืมหนังสือ</v-btn> <v-divider inset vertical></v-divider>
+        <v-btn text to="/remand">คืนหนังสือ</v-btn> <v-divider inset vertical></v-divider>
+        <v-btn text to="/rates">ให้คะแนน</v-btn> <v-divider inset vertical></v-divider>
+       <v-divider inset vertical></v-divider>
+
+
       </v-toolbar-items>
     </v-app-bar>
 
@@ -24,19 +38,9 @@
   </v-app>
 </template>
 
-<script>
 
 
-export default {
-  name: 'App',
 
-  components: {
-   
-  },
 
-  data: () => ({
-    //
-  }),
 
-};
-</script>
+
