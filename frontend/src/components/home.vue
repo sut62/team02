fb6fffd9d6a95782edec74df44d129ec533de717
unknown <kamonpan.b5923144@gmail.com>
2020-01-25 18:27:01 +0700

    แก้หน้าUIแสดงรายชื่อ - close #88

---
+++ frontend/src/components/home.vue
@@ -0,0 +1,15 @@
+<template>  <v-navigation-drawer
+     
+     
+      src="https://dcassetcdn.com/design_img/2495449/83570/83570_13222851_2495449_a5ee7223_image.png"
+      width="500%"
+      height="700"
+    
+    >
+
+  
+  </v-navigation-drawer>
+</template>
+
+
+
